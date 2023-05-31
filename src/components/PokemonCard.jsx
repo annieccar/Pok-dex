f4fc3ff103dc1432712457ca232b4c84e9a57822
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-05-31 09:59:18 +0200

    New branch created

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+const PokemonCard = () => {
+  return <figure>
+    <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image of a Balbusrs"/>
+    <figcaption>Balbusur</figcaption>
+    </figure>;
+};
+
+export default PokemonCard;
