2b5336a0c9827e29e67b3c7e83f3e432f436e814
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-05-31 12:31:25 +0200

    React quest 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import PokemonCard from "./components/PokemonCard";
 import "./App.css";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
-    <>
-      <div>
-        <PokemonCard />
-      </div>
-    </>
+    <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
   );
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,4 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const PokemonCard = () => {
-  const pokemon = pokemonList[0];
-
+const PokemonCard = (pokemon) => {
   if (pokemon.imgSrc) {
     return (
       <figure>
