14802397d5551a7238a2f179e331f073c7e5ed59
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-05-31 10:58:25 +0200

    React quest 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,32 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const PokemonCard = () => {
-  return <figure>
-    <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image of a Balbusrs"/>
-    <figcaption>Balbusur</figcaption>
-    </figure>;
+  const pokemon = pokemonList[0];
+
+  if (pokemon.imgSrc) {
+    return (
+      <figure>
+        <img src={pokemon.imgSrc} alt={`Image of a ${pokemon.name}`} />
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  } else {
+    return (
+      <figure>
+        <p>???</p>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  }
 };
 
 export default PokemonCard;
