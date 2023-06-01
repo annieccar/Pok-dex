61d8dbe63427d1e87e19ad17d8495fbe868249ec
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-06-01 18:12:22 +0200

    React quest 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import PokemonCard from "./components/PokemonCard";
 import "./App.css";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -8,17 +9,43 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [pokémonIndex, setPokémonIndex] = useState(0);
+  const handleIncrease = () => {
+    setPokémonIndex(pokémonIndex + 1);
+  };
+  const handleDecrease = () => {
+    setPokémonIndex(pokémonIndex - 1);
+  };
+
   return (
     <>
-      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
-      <PokemonCard name={pokemonList[1].name} imgSrc={pokemonList[1].imgSrc} />
+      <PokemonCard
+        name={pokemonList[pokémonIndex].name}
+        imgSrc={pokemonList[pokémonIndex].imgSrc}
+      />
+      <button onClick={handleDecrease}>Précédent</button>
+      <button onClick={handleIncrease}>Suivant</button>
     </>
-   
   );
 }
 
